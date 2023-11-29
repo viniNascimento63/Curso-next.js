d1fbb646e13cb21a81e71e5375a6f921463477ad
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-29 13:38:49 -0300

    Delete jsx3.js

--- exercicios/pages/basicos/jsx3.js
+++
@@ -1,9 +0,0 @@
-export default function jsx3() {
-    return (
-        <div>
-            <h1>JSX #03</h1>
-            <div id="legal"></div>
-            <span className="opa"></span>
-        </div>
-    )
-}
