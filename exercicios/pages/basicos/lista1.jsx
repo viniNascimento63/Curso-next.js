46f1335131a44afd660b34a37c9b5f6d0d846022
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-30 11:02:54 -0300

    Update lista1.jsx

--- exercicios/pages/basicos/lista1.jsx
+++ exercicios/pages/basicos/lista1.jsx
@@ -4,15 +4,15 @@ export default function lista1() {
         
         <div>
             <h1>Lista:</h1>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
+            <span>1</span><br/>
+            <span>2</span><br/>
+            <span>3</span><br/>
+            <span>4</span><br/>
+            <span>5</span><br/>
+            <span>6</span><br/>
+            <span>7</span><br/>
+            <span>8</span><br/>
+            <span>9</span><br/>
             <span>10</span>
         </div>
     );
