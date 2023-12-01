f7af80560c13683d48fa9637833e919a1a8c1a78
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-01 11:39:01 -0300

    Utilizando componente Titulo

---
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,12 @@
+// Utilizando components
+import Titulo from '../../components/titulo'
+
+export default function usandoTitulo() {
+    return (
+        <div>
+            <Titulo/>
+            <Titulo/>
+            <Titulo/>
+        </div>
+    )
+}
