218b6faee81fa305d19b82944687c04f87bc9594
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-06 13:20:36 -0300

    Update _app.js

--- exercicios/pages/_app.js
+++ exercicios/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import './css/integracao1.css'
+import './css/integracao1.css' // CSS de escopo global
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
